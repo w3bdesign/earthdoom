9fb9edb850c2ae8c4a56c5e1df744340293b1d72
Daniel <daniel@test.no>
2019-07-11 03:11:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            
+          Dette er en link
           </a>
         </header>
       </div>
