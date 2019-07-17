087983ae5754af2c59cfaff162b8830bb29cb9e5
Daniel <daniel@test.no>
2019-07-17 03:06:55 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,18 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-           Hei på deg
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-          Dette er en link
-          </a>
+          This is a temporary placeholder for the new Earthdoom website.
+          <br/>
+          Check back again soon and I will have new content up soon.
+          <br/>
+          (c) 2019
         </header>
       </div>
     );
