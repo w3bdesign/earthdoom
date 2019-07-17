b03615d9aa433fe650c5593555bb2586a4bd3a81
Daniel <daniel@test.no>
2019-07-17 03:18:10 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+       
           This is a temporary placeholder for the new Earthdoom website.
           <br/>
           Check back again soon and I will have new content up soon.
           <br/>
-          (c) 2019
-        </header>
+          Copyright 2019
+        
       </div>
     );
   }
