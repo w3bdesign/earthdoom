e8f6ed11d9b7e0d559f5cced41fe832e274731da
w3bdesign <hijack_xxx@yahoo.com>
2020-01-12 01:07:59 +0100

    Update test.js

--- test.js
+++ test.js
@@ -9,6 +9,11 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
+
+/*
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+});*/
