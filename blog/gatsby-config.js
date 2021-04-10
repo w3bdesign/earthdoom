660267ea8f288d6d6ddf32e176dfd4d7fbb4982f
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2021-04-10 23:05:21 +0200

    Replace with monorepo
    Gatsby with Sanity.io
    Nextjs with Hasura

---
+++ blog/gatsby-config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  siteMetadata: {
+    title: "Earthdoom Blog",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "zh1ax25r",
+        dataset: "production",
+      },
+    },
+    "gatsby-plugin-postcss",
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
