014fce7b8794b80e602019877223b644e685ccee
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-25 04:13:48 +0100

    Cleanup

--- game/src/pages/game.tsx
+++ game/src/pages/game.tsx
@@ -30,10 +30,7 @@ const Game: NextPage = () => {
               target="_blank"
             >
               <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
+             
             </Link>
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
