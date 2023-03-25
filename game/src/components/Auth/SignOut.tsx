59a9cb8c3e77f3e95b79f665024da441c201e565
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-25 06:17:50 +0100

    Update SignOut.tsx

--- game/src/components/Auth/SignOut.tsx
+++ game/src/components/Auth/SignOut.tsx
@@ -2,10 +2,6 @@ import { useClerk } from "@clerk/clerk-react";
 
 const SignOutButton = () => {
   const { signOut } = useClerk();
-  return (
-    <button onClick={() => signOut()} >
-      Sign out
-    </button>
-  );
+  return <button onClick={() => signOut()}>Sign out</button>;
 };
 export default SignOutButton;
