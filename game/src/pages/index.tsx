b0ab2381355e1a65b6a6914aa2d1d5ca72a8202b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-26 05:06:00 +0200

    Render pa users

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -27,7 +27,10 @@ const Home: NextPage = () => {
           </h1>
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
             Test: {JSON.stringify(secret.data)}
-            Pa user: {data?.toString()}
+            Pa users:
+            {data?.map((user) => (
+              <div key={user.id}>{user.name}</div>
+            ))}
             <Link
               className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
               href="https://create.t3.gg/en/usage/first-steps"
