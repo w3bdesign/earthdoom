0e62b859567fa3a82178980f798ef6daccff63d2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-27 21:41:11 +0200

    Test protected route

--- game/src/server/api/routers/paUsers.ts
+++ game/src/server/api/routers/paUsers.ts
@@ -1,7 +1,16 @@
-import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { createTRPCRouter, publicProcedure, privateProcedure } from "@/server/api/trpc";
 
 export const paUsersRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.paUsers.findMany();
   }),
+
+
+
+  getSecretMessage: privateProcedure.query(() => {
+    return "you can now see this secret message!";
+  }),
+
+
+
 });
