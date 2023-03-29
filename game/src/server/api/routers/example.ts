edf143c77c107858ebcd059b850ca228d28eaa4b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-03-29 17:31:36 +0200

    privateProcedure

--- game/src/server/api/routers/example.ts
+++ game/src/server/api/routers/example.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 import {
   createTRPCRouter,
   publicProcedure,
-  protectedProcedure,
+  privateProcedure,
 } from "@/server/api/trpc";
 
 export const exampleRouter = createTRPCRouter({
@@ -26,7 +26,7 @@ export const exampleRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.example.findMany();
   }),
-  getSecretMessage: protectedProcedure.query(() => {
+  getSecretMessage: privateProcedure.query(() => {
     return "you can now see this secret message!";
   }),
 });
