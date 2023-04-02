de0549a10df98f26a4763f680c0d3b5db9531ea2
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-02 07:49:22 +0200

    Fix design

--- game/src/pages/sats.tsx
+++ game/src/pages/sats.tsx
@@ -3,20 +3,15 @@ import { type NextPage } from "next";
 import Layout from "@/components/Layout/Layout";
 
 const Game: NextPage = () => {
-  
-
   return (
     <>
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <p className="text-2xl text-white">
-            
-            </p>
-           
+            <p className="text-2xl text-white"></p>
           </div>
         </div>
-        </Layout>
+      </Layout>
     </>
   );
 };
