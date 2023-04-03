c42958a7222a7037c2367b03d5edd3deb1879586
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-04 00:09:58 +0200

    Hostiles in Information

--- game/src/components/Header/Information.tsx
+++ game/src/components/Header/Information.tsx
@@ -1,9 +1,24 @@
+import { api } from "@/utils/api";
+import LoadingSpinner from "../Loader/LoadingSpinner";
+
 const Information = () => {
+  const { data: hostilesData } = api.paUsers.getHostiles.useQuery({
+    Userid: 1,
+  });
   return (
     <>
-      <div className="w-full flex flex-col items-center justify-center gap-12 px-4 py-4 mt-4 text-white">
-        <div className="flex flex-col items-center gap-2 text-lg text-center">
-          Here we will show game information
+      <div className="mt-4 flex w-full flex-col items-center justify-center gap-12 px-4 py-4 text-white">
+        <div className="flex flex-col items-center gap-2 text-center text-lg">
+          {hostilesData?.hostiles ? (
+            <div
+              className="mb-4 rounded-lg bg-danger-100 px-6 py-5 text-base text-danger-700"
+              role="alert"
+            >
+              {hostilesData.hostiles}
+            </div>
+          ) : (
+            <LoadingSpinner />
+          )}
         </div>
       </div>
     </>
