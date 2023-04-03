f351cef85dd425dfb6c8513649b35eab92d773d3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 02:27:19 +0200

    TS

--- game/src/next-env.d.ts
+++ game/src/next-env.d.ts
@@ -4,4 +4,5 @@
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
 
-declare module 'tw-elements/dist/plugin'
+declare module "tw-elements/dist/plugin";
+declare module "tw-elements";
