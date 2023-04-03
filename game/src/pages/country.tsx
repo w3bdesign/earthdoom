755c65ad72b97723e95938d082d8080da2354669
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 04:55:49 +0200

    Test responsive table

--- game/src/pages/country.tsx
+++ game/src/pages/country.tsx
@@ -221,6 +221,89 @@ const Country: NextPage = () => {
             </div>
           </div>
         </div>
+
+        <div className="mt-8 flex flex-col overflow-x-auto bg-white">
+          <div className="sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
+              <div className="overflow-x-auto">
+                <table className="min-w-full text-left text-sm font-light">
+                  <thead className="border-b font-medium dark:border-neutral-500">
+                    <tr>
+                      <th scope="col" className="px-6 py-4">
+                        #
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                      <th scope="col" className="px-6 py-4">
+                        Heading
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr className="border-b dark:border-neutral-500">
+                      <td className="whitespace-nowrap px-6 py-4 font-medium">
+                        1
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                    </tr>
+                    <tr className="border-b dark:border-neutral-500">
+                      <td className="whitespace-nowrap px-6 py-4 font-medium ">
+                        2
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4 ">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4 ">Cell</td>
+                    </tr>
+                    <tr className="border-b ">
+                      <td className="whitespace-nowrap px-6 py-4 font-medium ">
+                        3
+                      </td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                      <td className="whitespace-nowrap px-6 py-4">Cell</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
