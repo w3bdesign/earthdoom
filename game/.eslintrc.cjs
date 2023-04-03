c141c66383faa16aaa5b346682a54050d787115b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 02:42:48 +0200

    Update .eslintrc.cjs

--- game/.eslintrc.cjs
+++ game/.eslintrc.cjs
@@ -21,6 +21,9 @@ const config = {
   plugins: ["@typescript-eslint"],
   extends: ["next/core-web-vitals", "plugin:@typescript-eslint/recommended"],
   rules: {
+    "@typescript-eslint/no-unsafe-member-access": "warn",
+    "@typescript-eslint/no-floating-promises": "warn",
+
     "@typescript-eslint/consistent-type-imports": [
       "warn",
       {
