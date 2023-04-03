ffff4332cf98a85e281d5f8afeb18b5e5dbd8b7d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 03:14:15 +0200

    Update Layout.tsx

--- game/src/components/Layout/Layout.tsx
+++ game/src/components/Layout/Layout.tsx
@@ -17,7 +17,7 @@ const Layout = ({ children }: ILayoutProps) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
-      <main className="flex min-h-[79vh] xl:min-h-[80.8vh] items-center justify-center">
+      <main className="flex min-h-[79vh] items-center justify-center xl:min-h-[80.8vh]">
         {children}
       </main>
       <Footer />
