f129968a89a96c5371fa9595bde0e2b8d3307c54
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 06:04:52 +0200

    Font size

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
     <>
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 text-white">
-          <h1 className="text-center text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+          <h1 className="text-center text-3xl font-extrabold tracking-tight sm:text-[3rem]">
             Earth Doom
           </h1>
           <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-2 md:gap-8">
