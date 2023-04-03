d5e49be54c54043f296c65320922b194569d3043
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-03 06:16:20 +0200

    Show game information

---
+++ game/src/components/Header/Information.tsx
@@ -0,0 +1,13 @@
+const Information = () => {
+  return (
+    <>
+      <div className="w-full flex flex-col items-center justify-center gap-12 px-4 py-4 mt-4 text-white">
+        <div className="flex flex-col items-center gap-2 text-lg text-center">
+          Here we will show game information
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Information;
