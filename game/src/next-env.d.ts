451d41aeb7c97a7f7905c3f62f14da3ee0595b28
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-04 18:02:53 +0200

    Cleanup

--- game/src/next-env.d.ts
+++
@@ -1,8 +0,0 @@
-/// <reference types="next" />
-/// <reference types="next/image-types/global" />
-
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
-
-declare module "tw-elements/dist/plugin";
-declare module "tw-elements";
