2a330fca04e14f10931f88f7a91f686c633e9801
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-04 02:03:31 +0200

    Show email

--- game/src/components/Header/Information.tsx
+++ game/src/components/Header/Information.tsx
@@ -5,6 +5,13 @@ const Information = () => {
   const { data: hostilesData } = api.paUsers.getHostiles.useQuery({
     Userid: 1,
   });
+
+  const { data: paMail } = api.paMail.getMailByUserId.useQuery({
+    Userid: 1,
+  });
+
+  console.log("Epost:", paMail?.email);
+
   return (
     <>
       <div className="mt-4 flex w-full flex-col items-center justify-center gap-12 px-4 py-4 text-white">
@@ -15,6 +22,17 @@ const Information = () => {
               role="alert"
             >
               {hostilesData.hostiles}
+            </div>
+          ) : (
+            <LoadingSpinner />
+          )}
+
+          {paMail?.email ? (
+            <div
+              className="mb-4 rounded-lg bg-secondary-100 px-6 py-5 text-base text-secondary-800"
+              role="alert"
+            >
+              {JSON.stringify(paMail.email)}
             </div>
           ) : (
             <LoadingSpinner />
--- game/src/server/api/routers/paMail.ts
+++ game/src/server/api/routers/paMail.ts
@@ -1,7 +1,55 @@
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 
+import { z } from "zod";
+
+/*const User = z.object({
+  id: z.number(),
+  nick: z.string(),
+  md5: z.string(),
+});*/
+
+const Mail = z.object({
+  id: z.number(),
+  time: z.number(),
+  seen: z.number(),
+  header: z.string(),
+  news: z.string(),
+});
+
 export const paMailRouter = createTRPCRouter({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.paMail.findMany();
   }),
+
+  getMailByUserId: publicProcedure
+    .input(z.object({ Userid: z.number() }))
+    .query(async ({ ctx, input }) => {
+      const { Userid } = input;
+      console.log("Input: ", Userid);
+
+      const mails = await ctx.prisma.paMail.findMany({
+        where: {
+          id: Userid,
+          seen: 0,
+        },
+        orderBy: { time: "desc" },
+        take: 10,
+      });
+
+      console.log("Mails er: ", mails);
+
+      const epost = mails
+        .map((mail) =>
+          Mail.parse({
+            id: mail.id,
+            time: mail.time,
+            header: mail.header,
+            news: mail.news,
+            seen: mail.seen,
+          })
+        )
+        .join("");
+
+      return { email: mails[0] };
+    }),
 });
