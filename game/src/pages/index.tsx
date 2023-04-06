8aca6b32a6f15bd10dbf39d455a3826a72ce41ef
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-06 03:33:31 +0200

    Cleanup

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -1,15 +1,8 @@
 import { type NextPage } from "next";
 
-import { api } from "@/utils/api";
-
-import LoadingSpinner from "@/components/Loader/LoadingSpinner";
 import Layout from "@/components/Layout/Layout";
 
 const Home: NextPage = () => {
-  const hello = api.example.hello.useQuery({ text: "from tRPC" });
-
-  const { data: userData } = api.example.paUser.useQuery();
-
   return (
     <>
       <Layout>
@@ -17,19 +10,6 @@ const Home: NextPage = () => {
           <h1 className="text-center text-3xl font-extrabold tracking-tight sm:text-[3rem]">
             Earth Doom
           </h1>
-          <div className="grid grid-cols-1 gap-4 text-center sm:grid-cols-2 md:gap-8">
-            <h3 className="text-2xl font-bold">Name:</h3>
-            {userData?.map((user) => (
-              <div className="text-lg" key={user.id}>
-                {user.name}
-              </div>
-            ))}
-          </div>
-          <div className="flex flex-col items-center gap-2 bg-neutral-900">
-            <p className="p-4 text-center text-2xl">
-              {hello.data ? hello.data.greeting : <LoadingSpinner />}
-            </p>
-          </div>
         </div>
       </Layout>
     </>
