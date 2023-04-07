adcdaf46250b4dbbbfe5b578c16ffc18aad9a235
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-07 06:00:33 +0200

    Update paUsers.ts

--- game/src/server/api/routers/paUsers.ts
+++ game/src/server/api/routers/paUsers.ts
@@ -7,6 +7,18 @@ export const paUsersRouter = createTRPCRouter({
     return ctx.prisma.paUsers.findMany();
   }),
 
+  getAttackedPlayer: publicProcedure
+  .input(z.object({ Userid: z.number() }))
+  .query(async ({ ctx, input }) => {
+    const player = await ctx.prisma.paUsers.findUnique({
+      where: {
+        id: input.Userid,
+      },
+    });
+
+    return player;
+  }),
+
   getPlayerById: publicProcedure
     .input(z.object({ Userid: z.number() }))
     .query(async ({ ctx, input }) => {
