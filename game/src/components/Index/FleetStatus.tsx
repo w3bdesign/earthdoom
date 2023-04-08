5d6fe9cda91c46109ec23c890e9b1e68efbf7177
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-08 05:58:16 +0200

    Finish fleet status

--- game/src/components/Index/FleetStatus.tsx
+++ game/src/components/Index/FleetStatus.tsx
@@ -11,16 +11,46 @@ const FleetStatus: React.FC<FleetStatusProps> = ({ Userid }) => {
 
   if (!paPlayer) return null;
 
+  const { data: paAttackedName } = api.paUsers.getAttackedPlayer.useQuery({
+    Warid: paPlayer.war,
+  });
+
+  const { data: paDefendedName } = api.paUsers.getDefendedPlayer.useQuery({
+    Defid: paPlayer.def,
+  });
+
   return (
     <div className="mt-4 flex h-full w-full flex-col items-center justify-center">
       <h2 className="py-6 text-center text-2xl font-bold text-white">
         Fleet status
       </h2>
       <span className="mx-auto mb-10 text-lg text-white">
-        {paPlayer && paPlayer.war === 0 && "All fleets at home"}
-        {paPlayer &&
-          paPlayer.war < 0 &&
-          `Returning ... ETA ${paPlayer?.wareta}`}
+        {paPlayer.war === 0 && paPlayer.def === 0 && "All fleets at home"}
+        {paPlayer.war < 0 && `Returning ... ETA ${paPlayer?.wareta}`}
+
+        {paPlayer.war > 0 &&
+          paPlayer.wareta >= 5 &&
+          `Attacking ${paAttackedName?.nick} #${
+            paAttackedName?.id
+          } ${"  "}  (ETA:  ${paPlayer.wareta - 5} ticks)`}
+
+        {paPlayer.war > 0 &&
+          paPlayer.wareta < 5 &&
+          `Attacking ${paAttackedName?.nick} #${
+            paAttackedName?.id
+          } ${"  "}  (ETA: 0 ticks)`}
+
+        {paPlayer.def > 0 &&
+          paPlayer.defeta >= 5 &&
+          `Defending ${paDefendedName?.nick} #${
+            paDefendedName?.id
+          } ${"  "}  (ETA:  ${paPlayer.defeta - 5} ticks)`}
+
+        {paPlayer.def > 0 &&
+          paPlayer.defeta < 5 &&
+          `Defending ${paDefendedName?.nick} #${
+            paDefendedName?.id
+          } ${"  "}  (ETA: 0 ticks)`}
       </span>
     </div>
   );
