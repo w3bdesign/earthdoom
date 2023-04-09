64f79777356e403171173ddb32d42c734e792947
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-09 17:04:48 +0200

    Test enabled

--- game/src/components/Index/FleetStatus.tsx
+++ game/src/components/Index/FleetStatus.tsx
@@ -11,56 +11,60 @@ const FleetStatus: FC<FleetStatusProps> = ({ Userid }) => {
     Userid,
   });
 
-  if (paPlayer) {
-    const { data: paAttackedName } = api.paUsers.getAttackedPlayer.useQuery({
+  if (!paPlayer) return <h1>No player found</h1>;
+
+  const { data: paAttackedName } = api.paUsers.getAttackedPlayer.useQuery(
+    {
       Warid: paPlayer.war,
-    });
+    },
+    { enabled: paPlayer !== undefined }
+  );
 
-    const { data: paDefendedName } = api.paUsers.getDefendedPlayer.useQuery({
+  const { data: paDefendedName } = api.paUsers.getDefendedPlayer.useQuery(
+    {
       Defid: paPlayer.def,
-    });
+    },
+    { enabled: paPlayer !== undefined }
+  );
 
-    return (
-      <>
-        <div className="mt-4 flex h-full w-full flex-col items-center justify-center">
-          <h2 className="py-6 text-center text-2xl font-bold text-white">
-            Fleet status
-          </h2>
-          <span className="mx-auto mb-10 text-lg text-white">
-            {paPlayer.war === 0 && paPlayer.def === 0 && "All fleets at home"}
-            {paPlayer.war < 0 ||
-              (paPlayer.def < 0 && `Returning ... ETA ${paPlayer?.wareta}`)}
+  return (
+    <>
+      <div className="mt-4 flex h-full w-full flex-col items-center justify-center">
+        <h2 className="py-6 text-center text-2xl font-bold text-white">
+          Fleet status
+        </h2>
+        <span className="mx-auto mb-10 text-lg text-white">
+          {paPlayer.war === 0 && paPlayer.def === 0 && "All fleets at home"}
+          {paPlayer.war < 0 ||
+            (paPlayer.def < 0 && `Returning ... ETA ${paPlayer?.wareta}`)}
 
-            {paAttackedName &&
-              paPlayer.wareta >= 5 &&
-              `Attacking ${paAttackedName?.nick} #${
-                paAttackedName?.id
-              } ${"  "}  (ETA:  ${paPlayer.wareta - 5} ticks)`}
+          {paAttackedName &&
+            paPlayer.wareta >= 5 &&
+            `Attacking ${paAttackedName?.nick} #${
+              paAttackedName?.id
+            } ${"  "}  (ETA:  ${paPlayer.wareta - 5} ticks)`}
 
-            {paAttackedName &&
-              paPlayer.wareta < 5 &&
-              `Attacking ${paAttackedName?.nick} #${
-                paAttackedName?.id
-              } ${"  "}  (ETA: 0 ticks)`}
+          {paAttackedName &&
+            paPlayer.wareta < 5 &&
+            `Attacking ${paAttackedName?.nick} #${
+              paAttackedName?.id
+            } ${"  "}  (ETA: 0 ticks)`}
 
-            {paDefendedName &&
-              paPlayer.defeta >= 5 &&
-              `Defending ${paDefendedName?.nick} #${
-                paDefendedName?.id
-              } ${"  "}  (ETA:  ${paPlayer.defeta - 5} ticks)`}
+          {paDefendedName &&
+            paPlayer.defeta >= 5 &&
+            `Defending ${paDefendedName?.nick} #${
+              paDefendedName?.id
+            } ${"  "}  (ETA:  ${paPlayer.defeta - 5} ticks)`}
 
-            {paDefendedName &&
-              paPlayer.defeta < 5 &&
-              `Defending ${paDefendedName?.nick} #${
-                paDefendedName?.id
-              } ${"  "}  (ETA: 0 ticks)`}
-          </span>
-        </div>
-      </>
-    );
-  } else {
-    return <h1>No player found</h1>;
-  }
+          {paDefendedName &&
+            paPlayer.defeta < 5 &&
+            `Defending ${paDefendedName?.nick} #${
+              paDefendedName?.id
+            } ${"  "}  (ETA: 0 ticks)`}
+        </span>
+      </div>
+    </>
+  );
 };
 
 export default FleetStatus;
