14fc6b32b6b2b2801b8f2bcec9a16a43e81b384d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-09 20:31:32 +0200

    Cleanup

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -19,11 +19,7 @@ const Home: NextPage = () => {
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 text-white">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900 p-6">
-            <h1 className="text-2xl ">Main</h1>
-            <p className="text-base">
-              This page is a work in progress. It is very soon a finished
-              product.
-            </p>
+            <h1 className="text-center text-2xl">Main</h1>
             <div className="relative sm:mx-auto">
               {paPlayer && (
                 <>
