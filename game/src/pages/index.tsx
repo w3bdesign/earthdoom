dd0f087ffcfaeb0f034ebfc77abe854280984338
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-09 04:03:29 +0200

    WIP

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -13,6 +13,10 @@ const Home: NextPage = () => {
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 text-white">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900 p-6">
+            <h1 className="text-2xl ">Main</h1>
+            <p className="text-base">
+              This page is a work in progress. It is not a finished product.
+            </p>
             <div className="relative sm:mx-auto">
               <UnitsTable Userid={1} />
               <BDUTable Userid={1} />
