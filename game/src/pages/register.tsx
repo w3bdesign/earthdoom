3cef95ef624bb33f4b7b1c9e1297c3176ca735de
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-13 21:12:31 +0200

    Revert "Cleanup"
    This reverts commit 71337ac1f5a2593d1f1d6d7ec12c01899dd3d884.

---
+++ game/src/pages/register.tsx
@@ -0,0 +1,31 @@
+import { type NextPage } from "next";
+
+import { SignIn, SignUp, useUser } from "@clerk/nextjs";
+
+import SignOutButton from "@/components/Auth/SignOut";
+import Layout from "@/components/Layout/Layout";
+
+const Register: NextPage = () => {
+  const user = useUser();
+
+  return (
+    <>
+      <Layout>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">
+              {!user.isSignedIn && (
+                <SignIn path="/login" routing="path" signUpUrl="/login" redirectUrl="/afterlogin" />
+              )}
+              {user.isSignedIn && <SignOutButton />}
+              <br />
+              <SignUp path="/register" routing="path" signInUrl="/register" redirectUrl="/addUser" />
+            </p>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Register;
