cef72589e9376cdf238213ebc3050b6931fb26bd
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-13 04:33:13 +0200

    Update auth.ts

--- game/src/server/auth.ts
+++ game/src/server/auth.ts
@@ -4,9 +4,9 @@ import {
   type NextAuthOptions,
   type DefaultSession,
 } from "next-auth";
-
+import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
+import { env } from "@/env.mjs";
 import { prisma } from "@/server/db";
 
 /**
@@ -46,6 +46,18 @@ export const authOptions: NextAuthOptions = {
     },
   },
   adapter: PrismaAdapter(prisma),
+  providers: [
+   
+    /**
+     * ...add more providers here.
+     *
+     * Most other providers require a bit more work than the Discord provider. For example, the
+     * GitHub provider requires you to add the `refresh_token_expires_in` field to the Account
+     * model. Refer to the NextAuth.js docs for the provider you want to use. Example:
+     *
+     * @see https://next-auth.js.org/providers/github
+     */
+  ],
 };
 
 /**
