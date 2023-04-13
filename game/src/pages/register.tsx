7cac7f2531a7020fb135085d75b2d8f37dccea4c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-13 05:00:09 +0200

    Testing redirect

--- game/src/pages/register.tsx
+++ game/src/pages/register.tsx
@@ -15,11 +15,11 @@ const Register: NextPage = () => {
           <div className="flex flex-col items-center gap-2">
             <p className="text-2xl text-white">
               {!user.isSignedIn && (
-                <SignIn path="/login" routing="path" signUpUrl="/login" />
+                <SignIn path="/login" routing="path" signUpUrl="/login" redirectUrl="/afterlogin" />
               )}
               {user.isSignedIn && <SignOutButton />}
               <br />
-              <SignUp path="/register" routing="path" signInUrl="/register" />
+              <SignUp path="/register" routing="path" signInUrl="/register" redirectUrl="/addUser" />
             </p>
           </div>
         </div>
