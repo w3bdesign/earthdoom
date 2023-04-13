b002cde0a959ec2d58aa531331cd1681d2fb06a9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-13 23:22:16 +0200

    Fix authentication

--- game/src/pages/login/[[...index]].tsx
+++ game/src/pages/login/[[...index]].tsx
@@ -10,7 +10,12 @@ const Login: NextPage = () => {
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <SignIn path="/login" routing="path" signUpUrl="/login" />
+            <SignIn
+              path="/login"
+              routing="path"
+              //signUpUrl="/register"
+              //redirectUrl="/"
+            />
           </div>
         </div>
       </Layout>
