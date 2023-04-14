70e6d27d0862daba63e2329f07817b6810a61a62
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-14 03:06:17 +0200

    Create player after registration

--- game/src/pages/addUser/[[...index]].tsx
+++ game/src/pages/addUser/[[...index]].tsx
@@ -1,19 +1,44 @@
+import { useRouter } from "next/router";
+import { useUser } from "@clerk/nextjs";
+
 import { type NextPage } from "next";
 
 import Layout from "@/components/Layout/Layout";
 
-// import { api } from "@/utils/api";
+import { api } from "@/utils/api";
+import { useEffect } from "react";
 
 const AddUser: NextPage = () => {
+  const { user } = useUser();
+  const router = useRouter();
+
+  const { mutate } = api.paUsers.createPlayer.useMutation({
+    onSuccess() {
+      router.push("/");
+    },
+    onError() {
+      router.push("/error");
+    },
+  });
+
+  //if (!user) return null;
+
+  useEffect(() => {
+    if (user) {
+      mutate({ nick: user!.username! });
+    } else {
+      alert("No user");
+    }
+  }, [user?.username]);
+
   return (
     <>
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 text-white">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900 p-6">
-            <h1 className="text-center text-2xl">Main</h1>
-            <div className="relative sm:mx-auto">
-              Here we are going to add the user to the database
-            </div>
+            <h1 className="text-center text-2xl">Create player</h1>
+            <p className="text-center text-sm">User: {user?.username}</p>
+            <div className="relative sm:mx-auto">Creating player ...</div>
           </div>
         </div>
       </Layout>
