694d902ac510e370f198fc8e4fc46f8d5bed5890
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-14 03:05:55 +0200

    Error page

---
+++ game/src/pages/error.tsx
@@ -0,0 +1,19 @@
+import { type NextPage } from "next";
+
+import Layout from "@/components/Layout/Layout";
+
+const Error: NextPage = () => {
+  return (
+    <>
+      <Layout>
+        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+          <div className="flex flex-col items-center gap-2">
+            <p className="text-2xl text-white">Error page</p>
+          </div>
+        </div>
+      </Layout>
+    </>
+  );
+};
+
+export default Error;
