bb7239aa9db39b7699ff001259750710f1b1e1a5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-15 05:28:54 +0200

    Add toaster

--- game/src/pages/_app.tsx
+++ game/src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import Script from "next/script";
+import { Toaster } from "react-hot-toast";
 
 import { type AppType } from "next/app";
 
@@ -11,6 +12,7 @@ import "@/styles/globals.css";
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
   return (
     <ClerkProvider {...pageProps}>
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
       <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></Script>
     </ClerkProvider>
