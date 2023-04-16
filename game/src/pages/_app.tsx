243aa55e0e30e11542b87ac46db25500f40aa69a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-16 05:39:03 +0200

    Npm refresh

--- game/src/pages/_app.tsx
+++ game/src/pages/_app.tsx
@@ -1,12 +1,7 @@
 import Script from "next/script";
 import { Toaster } from "react-hot-toast";
-import { useRouter } from "next/router";
-import {
-  ClerkProvider,
-  SignedIn,
-  SignedOut,
-  RedirectToSignIn,
-} from "@clerk/nextjs";
+
+import { ClerkProvider } from "@clerk/nextjs";
 
 import { type AppType } from "next/app";
 
@@ -14,34 +9,10 @@ import { api } from "@/utils/api";
 
 import "@/styles/globals.css";
 
-//  List pages you want to be publicly accessible, or leave empty if
-
-//  every page requires authentication. Use this naming strategy:
-
-//   "/"              for pages/index.js
-
-//   "/foo"           for pages/foo/index.js
-
-//   "/foo/bar"       for pages/foo/bar.js
-
-//   "/foo/[...bar]"  for pages/foo/[...bar].js
-
-const publicPages = [
-  "/login/[[...index]]",
-  "/register/[[...index]]",
-  "/login",
-  "/register",
-];
-
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
-  const { pathname } = useRouter();
-
-  // Check if the current route matches a public page
-  const isPublicPage = publicPages.includes(pathname);
-
   return (
     <ClerkProvider {...pageProps}>
-      <Toaster position="bottom-center" />    
+      <Toaster position="bottom-center" />
       <Component {...pageProps} />
       <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></Script>
     </ClerkProvider>
