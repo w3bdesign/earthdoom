b4f29c14b7916a04a3d070448bf53ae1815c4d0c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-17 04:20:10 +0200

    Show news

--- game/src/pages/news.tsx
+++ game/src/pages/news.tsx
@@ -1,18 +1,45 @@
 import { useUser } from "@clerk/nextjs";
 
 import { type NextPage } from "next";
+import { type PaNews } from "@prisma/client";
 
 import { api } from "@/utils/api";
 
 import Layout from "@/components/Layout/Layout";
 import NewsTable from "@/components/News/NewsTable";
+import LoadingSpinner from "@/components/Loader/LoadingSpinner";
+
+interface IRenderContentProps {
+  news?: PaNews[];
+}
+
+const renderContent = (isLoading: boolean, paNews?: IRenderContentProps) => {
+  if (isLoading) {
+    return (
+      <div className="flex min-h-[150px] items-center justify-center">
+        <LoadingSpinner />
+      </div>
+    );
+  }
+
+  const hasNews = paNews?.news?.length ?? 0 > 0;
+  if (hasNews) {
+    return <NewsTable news={paNews?.news ?? []} />;
+  }
+
+  return (
+    <h1 className="text-bold p-4 text-center text-2xl text-black">
+      No news to report
+    </h1>
+  );
+};
 
 const News: NextPage = () => {
   const { user, isSignedIn } = useUser();
 
   if (!isSignedIn || !user.username) return null;
 
-  const { data: paNews } = api.paNews.getAllNewsByUserId.useQuery({
+  const { data: paNews, isLoading } = api.paNews.getAllNewsByUserId.useQuery({
     nick: user.username,
   });
 
@@ -21,17 +48,11 @@ const News: NextPage = () => {
       <Layout>
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <div className="flex flex-col items-center gap-2">
-            <div className="mt-8 flex flex-col bg-white text-black">
+            <div className="mt-8 flex min-h-[150px] min-w-[520px] flex-col bg-white text-black">
               <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full py-2 sm:px-6 lg:px-8">
                   <div className="overflow-hidden">
-                    {paNews?.news && paNews.news.length > 0 ? (
-                      <NewsTable paNews={paNews} />
-                    ) : (
-                      <h1 className="text-center text-2xl text-bold text-black p-4">
-                        No news to report
-                      </h1>
-                    )}
+                    {renderContent(isLoading, paNews)}
                   </div>
                 </div>
               </div>
