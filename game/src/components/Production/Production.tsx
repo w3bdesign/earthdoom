1e583bc4c4d9ed1d94cdcfe7cf468eb413fcb796
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-18 03:42:06 +0200

    Fix Typescript

--- game/src/components/Production/Production.tsx
+++ game/src/components/Production/Production.tsx
@@ -6,6 +6,7 @@ import { type FC } from "react";
 import { PRODUCTION } from "./constants/PRODUCTION";
 
 import { api } from "@/utils/api";
+import { IProduction } from "./types/types";
 
 interface PaPlayer {
   id: number;
@@ -22,14 +23,14 @@ interface PaPlayer {
 
 interface BuildingRowProps {
   paPlayer: PaPlayer;
-  building: any;
+  production: IProduction;
 }
 
 interface ConstructProps {
   paPlayer: PaPlayer;
 }
 
-const BuildingRow: FC<BuildingRowProps> = ({ paPlayer, building }) => {
+const BuildingRow: FC<BuildingRowProps> = ({ paPlayer, production }) => {
   const ctx = api.useContext();
   const { user, isLoaded } = useUser();
 
@@ -54,59 +55,56 @@ const BuildingRow: FC<BuildingRowProps> = ({ paPlayer, building }) => {
 
   return (
     <tr
-      key={building.buildingName}
+      key={production.buildingName}
       className="block border-b bg-white last:border-b-0 sm:table-row sm:border-none"
     >
       <td
         data-th="Name"
         className="flex h-12 items-center px-6 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
       >
-        {building.buildingName}
+        {production.buildingName}
       </td>
       <td
         data-th="Info"
         className="flex h-12 items-center px-6 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
       >
-        <span className="w-[12.5rem]">{building.buildingDescription}</span>
+        <span className="w-[12.5rem]">{production.buildingDescription}</span>
       </td>
       <td
         data-th="ETA"
         className="flex h-12 items-center px-6 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
       >
-        {paPlayer[building.buildingFieldName] >= 2
-          ? paPlayer[building.buildingFieldName] - 1
-          : building.buildingETA}
+        {paPlayer[production.buildingFieldName] >= 2
+          ? paPlayer[production.buildingFieldName] - 1
+          : production.buildingETA}
       </td>
       <td
         data-th="Production"
         className="flex h-12 items-center px-6 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
       >
         {isLoading && "Starting construction ..."}
-        {paPlayer[building.buildingFieldName] === 0 && !isLoading && (
+        {paPlayer[production.buildingFieldName] === 0 && !isLoading && (
           <>
             <input
               type="text"
               aria-label="Amount"
-              className="peer block min-h-[auto] w-32 relative rounded border-1 bg-slate-200 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+              className="border-1 peer relative block min-h-[auto] w-32 rounded bg-slate-200 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
               id="exampleFormControlInput1"
               placeholder="Amount"
             />
-           
           </>
         )}
 
-        {paPlayer[building.buildingFieldName] >= 2 && "Producing ..."}
-        {paPlayer[building.buildingFieldName] === 1 && "Done"}
+        {paPlayer[production.buildingFieldName] >= 2 && "Producing ..."}
+        {paPlayer[production.buildingFieldName] === 1 && "Done"}
       </td>
       <td
         data-th="Cost"
         className="flex h-12 items-center px-6 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
       >
-        {building.buildingCost}
+        {production.buildingCost}
       </td>
-      
     </tr>
-    
   );
 };
 
@@ -146,22 +144,20 @@ const BuildingTable: FC<ConstructProps> = ({ paPlayer }) => {
             Cost
           </th>
         </tr>
-        {PRODUCTION.map((building) => (
+        {PRODUCTION.map((production) => (
           <BuildingRow
-            key={building.buildingId}
+            key={production.buildingId}
             paPlayer={paPlayer}
-            building={building}
+            production={production}
           />
         ))}
       </tbody>
     </table>
-    
   );
 };
 
 const Production: FC<ConstructProps> = ({ paPlayer }) => (
   <BuildingTable paPlayer={paPlayer} />
-
 );
 
 export default Production;
