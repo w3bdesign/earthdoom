c69538e2c3c71de81fe17db9e93b1d3cdbfb11e0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-21 03:52:19 +0200

    Trpc to middleware

--- game/src/middleware.ts
+++ game/src/middleware.ts
@@ -5,7 +5,15 @@ export default withClerkMiddleware(() => {
   return NextResponse.next();
 });
 
-// Stop Middleware running on static files
 export const config = {
-  matcher: "/((?!_next/image|_next/static|favicon.ico).*)",
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next
+     * - static (static files)
+     * - favicon.ico (favicon file)
+     */
+    "/(.*?trpc.*?|(?!static|.*\\..*|_next|favicon.ico).*)",
+    "/",
+  ],
 };
