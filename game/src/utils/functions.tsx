24ff2c25c2680f36058b4cb05761ed4fccab47d3
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-24 05:39:46 +0200

    Render real income data

--- game/src/utils/functions.tsx
+++ game/src/utils/functions.tsx
@@ -1,4 +1,5 @@
 import type { PaPlayer } from "@/components/Production/Production";
+import { PaUsers } from "@prisma/client";
 
 interface IStringifierProps {
   value?: unknown;
@@ -22,7 +23,7 @@ interface IProduction {
  * @param {IStringifierProps} props - The props for the component
  * @returns {JSX.Element} The stringified value as a React element
  */
-export function Stringifier({ value }: IStringifierProps) {
+export const Stringifier = ({ value }: IStringifierProps) => {
   let stringifiedValue: string;
   // We check if the value is a string, number or bigint. If it is, we convert it to a string. Otherwise, we set it to an empty string.
   if (
@@ -35,7 +36,7 @@ export function Stringifier({ value }: IStringifierProps) {
     stringifiedValue = "";
   }
   return <span>{stringifiedValue}</span>;
-}
+};
 
 /**
  * Calculates the maximum number of units that can be trained based on the player's resources and the production cost of the unit.
@@ -82,4 +83,41 @@ export const canAffordToTrain = (
   );
 };
 
+/**
+ * Renders income data based on the given player object
+ * @param {PaUsers} paPlayer - The player object containing asteroid_metal and civilians properties
+ * @returns {Object} - The income data object containing labels, datasets and their respective data
+ */
+export const renderIncomeData = (paPlayer: PaUsers) => {
+  const tax = 20; // Set your tax value here
+  const sats = 5; // Set your sats value here
+  const extraTitanium = 1; // Set your extraTitanium value here
+  const extraCrystal = 1; // Set your extraCrystal value here
 
+  const civilians = paPlayer.civilians || 1000;
+  const metalroid = paPlayer.asteroid_metal;
+
+  const incomeCredits = Math.floor((civilians * tax) / 100);
+  const incomeCreditsWithBonus =
+    extraCrystal === 1
+      ? incomeCredits + Math.floor(incomeCredits * 0.1)
+      : incomeCredits;
+  const incomeTitanium =
+    metalroid * 60 + (extraTitanium === 1 ? Math.floor(metalroid * 0.1) : 0);
+  const incomeEnergy = sats * 45;
+
+  const data = {
+    labels: ["Titanium", "Credits", "Energy"],
+    datasets: [
+      {
+        label: "Income",
+        data: [incomeTitanium, incomeCreditsWithBonus, incomeEnergy],
+        backgroundColor: ["rgba(59, 113, 202, 1)"],
+        borderColor: ["rgba(255,255,255,1)"],
+        borderWidth: 2,
+      },
+    ],
+  };
+
+  return data;
+};
