813611e3c1c2fa005d7a4230c85dccea4f567493
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-24 17:45:19 +0200

    Npm refresh

--- game/src/components/Energy/constants/ENERGY.tsx
+++ game/src/components/Energy/constants/ENERGY.tsx
@@ -4,7 +4,7 @@ export const ENERGY = [
     buildingName: "Power plant",
     buildingDescription:
       "Power plants are used to generate energy. Energy is used to power your base and units.",
-    buildingFieldName: "sats",    
+    buildingFieldName: "sats",
     buildingETA: 10,
     buildingCost: "9500t",
     buildingCostCrystal: 0,
