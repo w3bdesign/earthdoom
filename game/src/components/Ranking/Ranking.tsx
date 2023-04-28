83ab1701cf961343d82d5b8af3a85e21f74f0a5c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-28 05:37:27 +0200

    Ranking

---
+++ game/src/components/Ranking/Ranking.tsx
@@ -0,0 +1,22 @@
+import type { FC } from "react";
+import type { PaUsers } from "@prisma/client";
+
+import DataTable from "@/components/common/DataTable";
+
+interface IRankingProps {
+  paPlayer: PaUsers;
+}
+
+const Ranking: FC<IRankingProps> = ({ paPlayer }) => {
+  const columns = [
+    { label: "Nick", accessor: "nick" },
+    { label: "Score", accessor: "score" },
+    { label: "Rank", accessor: "rank" },
+  ];
+
+  const caption = `Player ranking`;
+
+  return <DataTable columns={columns} data={paPlayer} caption={caption} />;
+};
+
+export default Ranking;
