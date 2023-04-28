1e11b224b07b4239b34dd58df4236f49a4129c59
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-28 05:45:01 +0200

    Add all imports

--- game/src/pages/military.tsx
+++ game/src/pages/military.tsx
@@ -1,14 +1,27 @@
+import { useUser } from "@clerk/nextjs";
+
 import { type NextPage } from "next";
 
 import Layout from "@/components/Layout/Layout";
+import Military from "@/components/Military/Military";
+
+import { api } from "@/utils/api";
 
 const MilitaryPage: NextPage = () => {
+  const { user, isSignedIn } = useUser();
+
+  if (!isSignedIn || !user.username) return null;
+
+  const { data: paPlayer } = api.paUsers.getPlayerById.useQuery({
+    nick: user.username,
+  });
+
   return (
     <>
       <Layout>
         <div className="container flex flex-col items-center justify-center px-2 py-2 ">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900">
-            <Military />
+            <Military paPlayer={paPlayer} />
           </div>
         </div>
       </Layout>
