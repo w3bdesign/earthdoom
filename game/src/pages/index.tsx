bbc9553739a067285b362b84b393b13145b0de9c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-04-30 03:14:14 +0200

    Styling and refactoring

--- game/src/pages/index.tsx
+++ game/src/pages/index.tsx
@@ -21,7 +21,7 @@ const Home = () => {
     <Layout>
       <div className="container flex flex-col items-center justify-center px-2 py-2 ">
         <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900">
-          <div className="relative py-4 sm:mx-auto">
+          <div className="relative sm:mx-auto">
             {paPlayer ? (
               <>
                 <UnitsTable paPlayer={paPlayer} />
