5a30ddcd00f16b99ff39b308e248b79feba73b6b
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-02 01:39:11 +0200

    Styling Military

--- game/src/components/features/Military/Military.tsx
+++ game/src/components/features/Military/Military.tsx
@@ -28,34 +28,34 @@ const Military: FC<IMilitaryProps> = ({ paPlayer }) => {
       <div className="w-full max-w-lg">
         <div className="mb-4 rounded-lg bg-white px-8 py-6 shadow-md">
           <h2 className="py-4 text-center text-xl font-bold">Attack:</h2>
-          <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
-            <span className="text-md">Country ID #:</span>
-
+          <form
+            onSubmit={handleSubmit}
+            className="mt-4 flex flex-col items-center justify-center py-2"
+          >
+            <span className="text-md mb-2">Country ID #:</span>
             <input
               type="text"
               name="attack"
               value={attack}
               onChange={(e) => setAttack(e.target.value)}
-              className="w-full rounded-md border border-gray-300 px-3 py-2"
+              className="w-64 rounded-md border border-gray-300 px-3 py-2"
             />
-            <div className="col-span-2">
-              <Button extraClasses="w-full">Attack</Button>
-            </div>
+            <Button extraClasses="w-32 mt-4">Attack</Button>
           </form>
           <h2 className="py-4 text-center text-xl font-bold">Defend:</h2>
-          <form onSubmit={handleSubmit} className="grid grid-cols-2 gap-4">
-            <span className="text-md">Country ID #:</span>
+          <form
+            onSubmit={handleSubmit}
+            className="mt-4 flex flex-col items-center justify-center py-2"
+          >
+            <span className="text-md mb-2">Country ID #:</span>
             <input
               type="text"
               name="defend"
               value={defend}
               onChange={(e) => setDefend(e.target.value)}
-              className="w-full rounded-md border border-gray-300 px-3 py-2"
+              className="w-64 rounded-md border border-gray-300 px-3 py-2"
             />
-
-            <div className="col-span-2">
-              <Button extraClasses="w-full">Defend</Button>
-            </div>
+            <Button extraClasses="w-32 mt-4">Defend</Button>
           </form>
         </div>
       </div>
