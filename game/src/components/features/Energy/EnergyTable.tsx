a074d4593a9ca10c97da81b78a263fb53d298e92
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-04 00:22:47 +0200

    Update EnergyTable.tsx

--- game/src/components/features/Energy/EnergyTable.tsx
+++ game/src/components/features/Energy/EnergyTable.tsx
@@ -60,7 +60,6 @@ const EnergyRow: FC<BuildingRowProps> = ({ paPlayer, energy }) => {
       >
         <span className="w-[12.5rem]">{energy.buildingDescription}</span>
       </td>
-
       <td
         data-th="Production"
         className="flex items-center px-6 py-2 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0 sm:table-cell  sm:border-l sm:border-t sm:before:content-none md:h-12"
