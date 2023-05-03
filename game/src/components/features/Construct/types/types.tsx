df2f36b27c4b280ea014918b21d3c944886f2f82
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-03 17:28:49 +0200

    Update types.tsx

--- game/src/components/features/Construct/types/types.tsx
+++ game/src/components/features/Construct/types/types.tsx
@@ -1,12 +1,12 @@
-export interface Building {
-  [key: string]: string | number;
-  buildingId: number;
-  buildingName: string;
-  buildingDescription: string;
-  buildingFieldName: string;
-  buildingETA: number;
-  buildingConstruct?: any;//JSX.Element;
-  buildingCost: string;
-  buildingCostCrystal: number;
-  buildingCostTitanium: number;
+export interface Building { 
+  [key: string]: string | number | JSX.Element | undefined;
+  buildingId: number; 
+  buildingName: string; 
+  buildingDescription: string; 
+  buildingFieldName: string; 
+  buildingETA: number; 
+  buildingConstruct?: JSX.Element; 
+  buildingCost: string; 
+  buildingCostCrystal: number; 
+  buildingCostTitanium: number; 
 }
