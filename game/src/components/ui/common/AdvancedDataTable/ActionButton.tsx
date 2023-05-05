8080625f9f480747436e3a6b827e3961365175b0
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-05 23:01:36 +0200

    Refactor and improve code

---
+++ game/src/components/ui/common/AdvancedDataTable/ActionButton.tsx
@@ -0,0 +1,99 @@
+import type { FC, RefObject } from "react";
+import type { Building } from "@/components/features/Construct/types/types";
+import type { PaPlayer } from "@/components/features/Production/Production";
+
+import Button from "../Button";
+import ToastComponent from "../ToastComponent";
+
+import { canAffordToTrain } from "@/utils/functions";
+import { TMutateType } from "./AdvancedDataTable";
+
+interface IActionButtonProps {
+  paPlayer: PaPlayer[];
+  building?: Building;
+  canAffordToTrain: typeof canAffordToTrain;
+  mutate: TMutateType;
+  actionText?: string;
+  actionInProgress?: string;
+  inputAmountRef?: RefObject<HTMLInputElement>;
+}
+
+ const ActionButton: FC<IActionButtonProps> = ({
+  paPlayer,
+  building,
+  canAffordToTrain,
+  mutate,
+  actionText,
+  actionInProgress = "Constructing ...",
+  inputAmountRef,
+}) => {
+  if (!paPlayer[0] || !building) return null;
+
+  const shouldNotCheckFieldName =
+    building.needsFieldName === 0 || building.needsFieldName === "undefined";
+
+  return (
+    <>
+      {(shouldNotCheckFieldName ||
+        paPlayer[0][building.buildingFieldName] === 0) && (
+        <Button
+          onClick={() => {
+            if (!paPlayer[0] || !paPlayer[0].id) return;
+
+            // Using a single condition to check for multiple values
+            const hasInputField =
+              Number(building.hasInputField) === 1 ||
+              building.hasInputField !== "undefined";
+
+            if (hasInputField && Number(inputAmountRef?.current?.value) === 0) {
+              ToastComponent({
+                message: "Quantity needs to be more than 0",
+                type: "error",
+              });
+              return;
+            }
+
+            // Using early returns to avoid nested if statements
+            if (
+              !canAffordToTrain(
+                paPlayer[0],
+                building.buildingCostCrystal,
+                building.buildingCostTitanium,
+                Number(inputAmountRef?.current?.value)
+              )
+            ) {
+              ToastComponent({
+                message: "You can not afford this",
+                type: "error",
+              });
+              return;
+            }
+
+            mutate({
+              Userid: Number(paPlayer[0].id),
+              buildingFieldName: building.buildingFieldName,
+              buildingETA: building.buildingETA,
+              buildingCostCrystal: building.buildingCostCrystal,
+              buildingCostTitanium: building.buildingCostTitanium,
+              unitAmount: Number(inputAmountRef?.current?.value),
+            });
+          }}
+        >
+          {actionText}
+        </Button>
+      )}
+      {paPlayer[0] &&
+        building &&
+        !shouldNotCheckFieldName &&
+        Number(paPlayer[0][building?.buildingFieldName]) >= 2 &&
+        `${actionInProgress}`}
+      {paPlayer[0] &&
+        building &&
+        !shouldNotCheckFieldName &&
+        paPlayer[0][building.buildingFieldName] === 1 &&
+        "Done"}
+    </>
+  );
+};
+
+export default ActionButton;
---
+++ game/src/components/ui/common/AdvancedDataTable/InputNumber.tsx
@@ -0,0 +1,31 @@
+import {  canAffordToTrain } from "@/utils/functions";
+
+import type { FC, RefObject } from "react";
+
+interface IInputNumberProps {
+  canAffordToTrain: typeof canAffordToTrain;
+  inputAmountRef: RefObject<HTMLInputElement>;
+}
+
+
+ const InputNumber: FC<IInputNumberProps> = ({ inputAmountRef }) => {
+  return (
+    <td
+      data-th="Production"
+      className="flex items-center px-6 py-2 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0 sm:table-cell  sm:border-l sm:border-t sm:before:content-none md:h-12"
+    >
+      <input
+        type="number"
+        aria-label="Amount"
+        className="border-1 peer relative block min-h-[auto] w-32 rounded bg-slate-200 px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-neutral-200 dark:placeholder:text-neutral-200 dark:peer-focus:text-primary [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
+        id="exampleFormControlInput1"
+        placeholder="Amount"
+        ref={inputAmountRef}
+        defaultValue={0}
+        min="0"
+      />
+    </td>
+  );
+};
+
+export default InputNumber;
