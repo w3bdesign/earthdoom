2fee25151be102e993c79ecceb521ce3e9fb60ac
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-06 03:11:06 +0200

    Update Alliance.tsx

--- game/src/components/features/Alliance/Alliance.tsx
+++ game/src/components/features/Alliance/Alliance.tsx
@@ -127,7 +127,6 @@ const Alliance: FC<IAllianceProps> = ({ paPlayer, paTag }) => {
                             });
                             return;
                           }
-
                           createAlliance({
                             Userid: paPlayer.id,
                             tagName: createAllianceRef.current.value,
