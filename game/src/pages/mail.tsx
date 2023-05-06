675270f7cd8c0ee93df050fdd18bf641606af56c
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-06 04:16:10 +0200

    Add Mail heading text

--- game/src/pages/mail.tsx
+++ game/src/pages/mail.tsx
@@ -26,7 +26,10 @@ const Mail: NextPage = () => {
       <Layout>
         <div className="container mb-6 flex flex-col items-center justify-center">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900">
-            <div className="mt-8 flex flex-col bg-white text-black">
+            <h2 className="py-4 text-center text-2xl font-bold text-white">
+              Mail
+            </h2>
+            <div className="mt-2 flex flex-col bg-white text-black">
               <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div className="inline-block min-w-full sm:px-6 lg:px-8">
                   <div className="overflow-hidden">
