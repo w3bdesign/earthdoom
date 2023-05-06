f9f6ad04e5420d2c9dcc4886c7c4a2e0c6e5ba70
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-06 04:07:31 +0200

    More work on Mail

---
+++ game/src/components/features/Mail/MailTable.tsx
@@ -0,0 +1,97 @@
+import { Button, ToastComponent } from "@/components/ui/common";
+
+import type { FC } from "react";
+import type { PaMail } from "@prisma/client";
+
+import { api } from "@/utils/api";
+
+interface IMailTableProps {
+  mail?: PaMail[];
+}
+
+const MailTable: FC<IMailTableProps> = ({ mail }) => {
+  const ctx = api.useContext();
+
+  const { data: allMail } = api.paMail.getAllMailByUserId.useQuery({
+    Userid: 12,
+  });
+
+  const { mutate: deleteSingleMail } = api.paMail.deleteEmail.useMutation({
+    onSuccess: async () => {
+      ToastComponent({ message: "Mail deleted", type: "success" });
+      await ctx.paMail.getAllMailByUserId.invalidate({ Userid: 1 });
+    },
+    onError: () => {
+      console.error("Failure deleting!");
+    },
+  });
+
+  return (
+    <>
+      {allMail && allMail.email.length > 0 && (
+        <table className="min-w-full text-left text-sm font-light">
+          <thead className="border-b font-medium dark:border-neutral-500">
+            <tr>
+              <th
+                scope="col"
+                className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell"
+              >
+                ID
+              </th>
+              <th
+                scope="col"
+                className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell"
+              >
+                Title
+              </th>
+              <th
+                scope="col"
+                className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell"
+              >
+                Content
+              </th>
+              <th
+                scope="col"
+                className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell"
+              >
+                Delete
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {allMail?.email.map((mail) => (
+              <tr key={mail.id} className="border-b dark:border-neutral-500">
+                <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+                  {mail.id}
+                </td>
+                <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+                  {mail.header}
+                </td>
+                <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+                  {mail.news}
+                </td>
+                <td className="flex h-12 items-center px-6 py-2 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':']  first:border-l-0 sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      deleteSingleMail({ id: mail.id });
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+      {allMail?.email.length === 0 && (
+        <h1 className="text-bold p-4 text-center text-2xl text-black">
+          No email to display
+        </h1>
+      )}
+    </>
+  );
+};
+
+export default MailTable;
