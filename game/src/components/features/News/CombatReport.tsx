2a2bfb4a1c3cbf359a959b3b93f685339a9e1108
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-08 00:58:07 +0200

    Update CombatReport.tsx

--- game/src/components/features/News/CombatReport.tsx
+++ game/src/components/features/News/CombatReport.tsx
@@ -1,66 +1,100 @@
 interface CombatReportProps {
-  data: {
-    title: string;
-    defenders: Record<string, { total: number; lost: string }>;
-    attackers: Record<string, { total: number; lost: string }>;
-    yours: Record<string, string | number>;
+  title: string;
+  defenders: {
+    [key: string]: {
+      total: number;
+      lost: string;
+    };
+  };
+  attackers: {
+    [key: string]: {
+      total: number;
+      lost: string;
+    };
+  };
+  yours: {
+    [key: string]: number | string;
   };
 }
 
-interface Props {
-  data: {
-    news: {
-      id: number;
-      sentTo: number;
-      time: number;
-      news: string;
-      seen: string;
-      header: string;
-    }[];
-  };
-}
+const CombatReport: React.FC<CombatReportProps> = ({
+  title,
+  defenders,
+  attackers,
+  yours,
+}) => {
+  const renderRow = (name: string, data: { total: number; lost: string }) => (
+    <tr key={name}>
+      <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+        {name}
+      </td>
+      <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+        {data.total}
+      </td>
+      <td className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+        {data.lost}
+      </td>
+    </tr>
+  );
 
-const CombatReport = ({ data }: Props) => {
-  console.log("Combatreport news: ", data);
-
-  //if (!data) return;
-
-  const tableHtml = `
-  <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Defenders</th>
-        <th>Attackers</th>
-        <th>Yours</th>
-        <th>Time</th>
-        <th>Seen</th>
-        <th>Header</th>
-      </tr>
-    </thead>
-    <tbody>
-      ${data.news
-        .map(
-          (item) => `
-        <tr>
-          <td>${item.id}</td>
-          <td>${JSON.parse(item.news).title}</td>
-          <td>${JSON.stringify(JSON.parse(item.news).defenders)}</td>
-          <td>${JSON.stringify(JSON.parse(item.news).attackers)}</td>
-          <td>${JSON.stringify(JSON.parse(item.news).yours)}</td>
-          <td>${item.time}</td>
-          <td>${item.seen}</td>
-          <td>${item.header}</td>
+  return (
+    <table className="w-[20.625rem] py-6 text-left ring-1 ring-slate-400/10 md:w-full">
+      <caption className="py-4 text-center text-xl font-bold text-black">
+        {title}
+      </caption>
+      <thead>
+        <tr className="block border-b bg-white p-4 last:border-b-0 sm:table-row sm:border-none md:p-0">
+          <th className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell">
+            Unit
+          </th>
+          <th className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell">
+            Total
+          </th>
+          <th className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell">
+            Lost
+          </th>
         </tr>
-      `
-        )
-        .join("")}
-    </tbody>
-  </table>
-`;
-
-  return <h1>Test: {tableHtml}</h1>;
+      </thead>
+      <tbody>
+        <tr className="block border-b p-4 last:border-b-0 sm:table-row sm:border-none md:p-0">
+          <td className="flex h-12 items-center bg-slate-400/90 px-6 text-center text-base font-bold text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+            Defenders
+          </td>
+          <td className="bg-slate-400/90"></td>
+          <td className="bg-slate-400/90"></td>
+        </tr>
+        {Object.entries(defenders).map(([name, data]) => renderRow(name, data))}
+        <tr className="mt-4 block border-b p-4 last:border-b-0 sm:table-row sm:border-none md:p-0">
+          <td className="mt-4 flex h-12 items-center bg-slate-400/90 px-6 text-center text-base font-bold text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+            Attackers
+          </td>
+          <td className="bg-slate-400/90"></td>
+          <td className="bg-slate-400/90"></td>
+        </tr>
+        {Object.entries(attackers).map(([name, data]) => renderRow(name, data))}
+        <tr className="block border-b bg-white p-4 last:border-b-0 sm:table-row sm:border-none md:p-0">
+          <td className="flex h-12 items-center bg-slate-400/90 px-6 text-center text-base font-bold text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+            Yours
+          </td>
+          <td className="bg-slate-400/90"></td>
+          <td className="bg-slate-400/90"></td>
+        </tr>
+        {Object.entries(yours).map(([name, value]) => (
+          <tr
+            className="block bg-white p-4 last:border-b-0 sm:table-row sm:border-none md:p-0"
+            key={name}
+          >
+            <td className=" flex h-12 items-center border-r px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+              {name}
+            </td>
+            <td className="flex h-12 items-center border-r px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none">
+              {value}
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
 };
 
 export default CombatReport;
