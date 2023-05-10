7b89a2bba789cfca03627c388ffea891c98c0806
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-11 01:55:10 +0200

    Fix menu bug on load

--- game/src/components/common/Header/Navbar.tsx
+++ game/src/components/common/Header/Navbar.tsx
@@ -3,24 +3,25 @@ import Link from "next/link";
 import Script from "next/script";
 
 import { LINKS } from "./constants/LINKS";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [expanded, setExpanded] = useState<boolean>(false);
+
   return (
     <div className="flex min-h-[6.875rem] justify-center bg-gray-800 p-6">
       <SignedIn>
-        <nav
-          className="relative flex w-full items-center justify-between bg-gray-800 py-2 text-white  dark:bg-neutral-700 dark:text-neutral-300 lg:flex-wrap lg:justify-center"
-          data-te-navbar-ref
-        >
+        <nav className="relative flex w-full items-center justify-between bg-gray-800 py-2 text-white  dark:bg-neutral-700 dark:text-neutral-300 lg:flex-wrap lg:justify-center">
           <div className="px-6">
             <button
               className="border-0 text-xl leading-none transition-shadow duration-150 ease-in-out dark:hover:text-white dark:focus:text-white lg:hidden"
               type="button"
-              data-te-collapse-init
-              data-te-target="#navbarSupportedContentX"
               aria-controls="navbarSupportedContentX"
               aria-expanded="false"
               aria-label="Toggle navigation"
+              onClick={() => {
+                setExpanded(!expanded);
+              }}
             >
               <span className="[&>svg]:w-8">
                 <svg
@@ -42,21 +43,17 @@ const Navbar = () => {
             <div
               className="!visible hidden flex-grow basis-[100%] items-center lg:!flex lg:basis-auto"
               id="navbarSupportedContentX"
-              data-te-collapse-item
             >
-              <ul className="mr-auto flex flex-row" data-te-navbar-nav-ref>
-                <li
-                  className="static"
-                  data-te-nav-item-ref
-                  data-te-dropdown-ref
-                >
+              <ul className="mr-auto flex flex-row">
+                <li className="static">
                   <button
                     className="flex items-center whitespace-nowrap py-2 pr-2 text-lg font-bold  transition duration-150 ease-in-out hover:text-slate-300  dark:hover:text-white dark:focus:text-white lg:px-2"
                     type="button"
                     id="dropdownMenuButtonX"
-                    data-te-dropdown-toggle-ref
                     aria-expanded="false"
-                    data-te-nav-link-ref
+                    onClick={() => {
+                      setExpanded(!expanded);
+                    }}
                   >
                     Main menu
                     <span className="ml-2 w-2">
@@ -75,9 +72,11 @@ const Navbar = () => {
                     </span>
                   </button>
                   <div
-                    className="absolute left-0 right-0 top-full z-[1000] mt-0 hidden w-full border-none bg-white bg-clip-padding text-neutral-600  dark:bg-neutral-700 dark:text-neutral-200 [&[data-te-dropdown-show]]:block"
+                    className={`                    
+                    absolute left-0 right-0 top-full z-[1000] mt-0 ${
+                      expanded ? "" : "hidden"
+                    } w-full border-none bg-white bg-clip-padding text-neutral-600  dark:bg-neutral-700 dark:text-neutral-200 [&[data-te-dropdown-show]]:block`}
                     aria-labelledby="dropdownMenuButtonX"
-                    data-te-dropdown-menu-ref
                   >
                     <div className="px-6 py-5 lg:px-8">
                       <div className="grid gap-4 md:grid-cols-2 lg:grid-cols-4">
@@ -100,7 +99,6 @@ const Navbar = () => {
           </div>
         </nav>
       </SignedIn>
-      <Script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js" />
     </div>
   );
 };
