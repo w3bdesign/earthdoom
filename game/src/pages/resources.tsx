4d6c7065a27d35adedc4a737f72e94458f095acd
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-13 00:01:24 +0200

    Income styling

--- game/src/pages/resources.tsx
+++ game/src/pages/resources.tsx
@@ -67,7 +67,7 @@ const Resources: NextPage = () => {
         <div className="container mb-6 flex flex-col items-center justify-center">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900">
             {!isLoaded && <LoadingSpinner />}
-            <div className="mb-4 mt-6 md:w-[44.563rem] rounded bg-white py-4 shadow">
+            <div className="mb-4 mt-6 rounded bg-white py-4 shadow mx-auto md:w-[44.563rem]">
               {hasNoLand ? (
                 <h2 className="p-4 text-center text-2xl font-bold">
                   No land, no income.
