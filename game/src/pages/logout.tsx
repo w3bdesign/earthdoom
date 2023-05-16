b0b043a79f492670a77061e465605e847ff034ff
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-16 04:30:17 +0200

    Fix hook render bug

--- game/src/pages/logout.tsx
+++ game/src/pages/logout.tsx
@@ -11,7 +11,7 @@ const Logout: NextPage = () => {
       <Layout>
         <div className="container mb-6 flex flex-col items-center justify-center">
           <div className="relative flex flex-col justify-center overflow-hidden bg-neutral-900">
-            <p className="text-2xl text-white mt-12">
+            <p className="mt-12 text-2xl text-white">
               <SignedIn>
                 <SignUpButton>
                   <Button
