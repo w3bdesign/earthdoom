555102875f123286a00f10972e4779c7de5e23d9
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-16 02:14:57 +0200

    Jsdoc

--- game/src/utils/hooks.ts
+++ game/src/utils/hooks.ts
@@ -1,5 +1,12 @@
 import { createRef, RefObject } from "react";
 
+/**
+ * Creates an array of RefObjects for the specified length.
+ *
+ * @template T - The type of HTMLInputElement the RefObject will reference.
+ * @param {number} length - The number of RefObjects to create.
+ * @returns {RefObject<T>[]} - An array of RefObjects of the specified length.
+ */
 export const useMultipleRefs = <T extends HTMLInputElement>(
   length: number
 ): RefObject<T>[] => {
