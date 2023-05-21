522ee2bbdec8444c0181560c695c5b1d2e6b63ff
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-21 04:16:14 +0200

    Revert "Refactor"
    This reverts commit 84d7f4cade56eb271a51fec5664dac7a1e94ef44.

--- game/src/components/ui/tables/DataTable.tsx
+++ game/src/components/ui/tables/DataTable.tsx
@@ -17,7 +17,11 @@ export interface DataTableProps {
  * @param {DataTableProps} props - The props for the DataTable component
  * @returns {JSX.Element} - The DataTable component
  */
-const DataTable: React.FC<DataTableProps> = ({ columns, data, caption }) => {
+export const DataTable: React.FC<DataTableProps> = ({
+  columns,
+  data,
+  caption,
+}) => {
   return (
     <table className="mt-4 w-[20.625rem] text-left ring-1 ring-slate-400/10 md:w-full">
       <caption className="py-6 text-center text-2xl font-bold text-white">
@@ -25,13 +29,13 @@ const DataTable: React.FC<DataTableProps> = ({ columns, data, caption }) => {
       </caption>
       <thead>
         <tr>
-          {columns.map((column, index) => (
+          {columns.map((col, index) => (
             <th
               key={index}
               scope="col"
               className="hidden h-12 bg-slate-200/90 px-6 text-center text-base font-bold text-black first:border-l-0 sm:table-cell"
             >
-              {column.label}
+              {col.label}
             </th>
           ))}
         </tr>
@@ -42,13 +46,13 @@ const DataTable: React.FC<DataTableProps> = ({ columns, data, caption }) => {
             key={rowIndex}
             className="block rounded bg-white p-4 shadow last:border-b-0 sm:table-row sm:border-none md:p-0"
           >
-            {columns.map((column, colIndex) => (
+            {columns.map((col, colIndex) => (
               <td
                 key={colIndex}
-                data-th={column.label}
+                data-th={col.label}
                 className="flex h-12 items-center px-6 text-center text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0  sm:table-cell sm:border-l sm:border-t sm:before:content-none"
               >
-                <Stringifier value={row[column.accessor]} />
+                <Stringifier value={row[col.accessor]} />
               </td>
             ))}
           </tr>
@@ -57,5 +61,3 @@ const DataTable: React.FC<DataTableProps> = ({ columns, data, caption }) => {
     </table>
   );
 };
-
-export default DataTable;
