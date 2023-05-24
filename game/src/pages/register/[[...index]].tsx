92faae5f544b2a4bdf6088380e9221a0a034bf17
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-05-24 02:14:46 +0200

    JSDoc

--- game/src/pages/register/[[...index]].tsx
+++ game/src/pages/register/[[...index]].tsx
@@ -4,6 +4,11 @@ import { UserButton, SignUp, SignedIn, SignedOut } from "@clerk/nextjs";
 
 import { Layout } from "@/components/common/Layout";
 
+/**
+ * Renders the Register component that displays a form for users to sign up. 
+ *
+ * @return {JSX.Element} The JSX element representing the Register component.
+ */
 const Register: NextPage = () => {
   return (
     <>
