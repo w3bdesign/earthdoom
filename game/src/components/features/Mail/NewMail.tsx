8115b9e08cc3ca1d1de07a08ad3471de9ad8b87a
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-06-29 06:54:14 +0200

    Aria and refactoring

--- game/src/components/features/Mail/NewMail.tsx
+++ game/src/components/features/Mail/NewMail.tsx
@@ -48,55 +48,61 @@ const NewMail: FC<IMilitaryProps> = ({ paPlayer }) => {
     setMailHeader(event.target.value);
   };
 
+  const handleMailSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+
+    sendMail({
+      nick: mailTarget,
+      news: mailContent,
+      header: mailHeader,
+    });
+  };
+
   return (
     <div className="flex flex-col items-center justify-center py-4">
       <div className="w-full">
-        <div className="mb-4 rounded-lg bg-white px-8 py-5 shadow-md">
-          <h2 className="py-4 text-center text-xl font-bold">Mail</h2>
-          <div className="flex flex-col items-center justify-center">
-            <span className="text-md py-4">Nick:</span>
-            <input
-              type="text"
-              name="attack"
-              onChange={handleInputMailTargetChange}
-              className="w-64 rounded-md border border-gray-300 px-3 py-2"
-              required
-            />
-            <span className="text-md py-4">Title:</span>
-            <input
-              type="text"
-              name="attack"
-              onChange={handleInputMailHeaderChange}
-              className="w-64 rounded-md border border-gray-300 px-3 py-2"
-              required
-            />
-
-            <span className="text-md py-4">Content:</span>
-            <textarea
-              name="attack"
-              onChange={handleInputMailContentChange}
-              className="w-64 rounded-md border border-gray-300 px-3 py-2"
-              required
-            />
-            <Button
-              disabled={isLoading}
-              onClick={(event) => {
-                event.preventDefault();
-
-                console.log("mailTarget", mailTarget);
-
-                sendMail({
-                  nick: mailTarget,
-                  news: mailContent,
-                  header: mailHeader,
-                });
-              }}
-              extraClasses="w-32 mt-4"
-            >
-              Send
-            </Button>
+        <form onSubmit={handleMailSubmit}>
+          <div className="mb-4 rounded-lg bg-white px-8 py-5 shadow-md">
+            <h2 className="py-4 text-center text-xl font-bold">Mail</h2>
+            <div className="flex flex-col items-center justify-center">
+              <label className="text-md py-4" htmlFor="nick">
+                Nick:
+              </label>
+              <input
+                type="text"
+                id="nick"
+                name="attack"
+                onChange={handleInputMailTargetChange}
+                className="w-64 rounded-md border border-gray-300 px-3 py-2"
+                required
+              />
+              <label className="text-md py-4" htmlFor="title">
+                Title:
+              </label>
+              <input
+                type="text"
+                id="title"
+                name="attack"
+                onChange={handleInputMailHeaderChange}
+                className="w-64 rounded-md border border-gray-300 px-3 py-2"
+                required
+              />
+              <label className="text-md py-4" htmlFor="content">
+                Content:
+              </label>
+              <textarea
+                id="content"
+                name="attack"
+                onChange={handleInputMailContentChange}
+                className="w-64 rounded-md border border-gray-300 px-3 py-2"
+                required
+              />
+              <Button disabled={isLoading} extraClasses="w-32 mt-4">
+                Send
+              </Button>
+            </div>
           </div>
-        </div>
+        </form>
       </div>
     </div>
   );
