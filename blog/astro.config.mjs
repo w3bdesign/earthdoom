69988bea210eeecc56e9bda8446b27dd749c84ce
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-08 03:29:44 +0200

    Astro Vercel

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -3,8 +3,12 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel/serverless";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.earthdoom.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [mdx(), sitemap(), tailwind()],
+  output: "server",
+  adapter: vercel()
 });
