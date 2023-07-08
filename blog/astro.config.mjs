882bb5b4194863f4c5a2e39ab989adc81a93e6f5
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-08 03:14:29 +0200

    Update astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://earthdoom.com',
+  site: 'https://www.earthdoom.com',
   integrations: [mdx(), sitemap(), tailwind()]
 });
