2d255cf265e2070dd7e45b1a69361a059e3fcf12
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-10 04:06:27 +0200

    Update astro.config.mjs

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -9,6 +9,6 @@ import vercel from "@astrojs/vercel/serverless";
 export default defineConfig({
   site: 'https://www.earthdoom.com',
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "static",
+  output: "hybrid",
   adapter: vercel()
 });
