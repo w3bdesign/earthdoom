f6e3b94079f981fde7341ec0b6735ebfb5c94028
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-10 04:30:09 +0200

    Static

--- blog/astro.config.mjs
+++ blog/astro.config.mjs
@@ -1,18 +1,11 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.earthdoom.com',
-<<<<<<< HEAD
+  site: "https://www.earthdoom.com",
   integrations: [mdx(), sitemap(), tailwind()],
-  output: "hybrid",
-  adapter: vercel()
-=======
-  integrations: [mdx(), sitemap(), tailwind()],  
-  output: "static" 
->>>>>>> parent of 776d563 (Update astro.config.mjs)
+  output: "static",
 });
