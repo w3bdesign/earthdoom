1b9bf89b677d26140a505956ea8d0f4314d784b6
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2023-07-21 19:16:39 +0200

    Update Production.tsx

--- game/src/components/features/Production/Production.tsx
+++ game/src/components/features/Production/Production.tsx
@@ -101,7 +101,8 @@ const ProductionRow: FC<BuildingRowProps> = ({ paPlayer, production }) => {
         data-th="Build"
         className="flex items-center px-6 py-2 text-base text-black transition duration-300 before:inline-block before:w-24 before:font-medium before:text-black before:content-[attr(data-th)':'] first:border-l-0 sm:table-cell  sm:border-l sm:border-t sm:before:content-none md:h-12"
       >
-        {isLoading && "Starting ..."}
+        {isLoading && `<div class="py-2">Starting ...</div>`}
+        <br />
         {paPlayer[production.buildingFieldName] === 0 && !isLoading && (
           <Button
             onClick={() => {
