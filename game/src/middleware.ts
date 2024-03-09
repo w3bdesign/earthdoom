c2c730e803df71bd900dc692d5387489d43afbdc
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-03-09 07:51:49 +0100

    .

--- game/src/middleware.ts
+++ game/src/middleware.ts
@@ -1,8 +1,19 @@
-import { withClerkMiddleware } from "@clerk/nextjs/server";
-import { NextResponse } from "next/server";
+//import { withClerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs";
+//import { NextResponse } from "next/server";
 
+/*
 export default withClerkMiddleware(() => {
   return NextResponse.next();
+});
+*/
+
+export default authMiddleware({
+  // Routes that can be accessed while signed out
+  publicRoutes: ["/anyone-can-visit-this-route"],
+  // Routes that can always be accessed, and have
+  // no authentication information
+  ignoredRoutes: ["/no-auth-in-this-route"],
 });
 
 export const config = {
