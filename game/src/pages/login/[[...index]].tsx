e4d2c79f4d12903130eadd5e61fdb090ff50f1b0
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-21 04:48:56 +0200

    Update [[...index]].tsx

--- game/src/pages/login/[[...index]].tsx
+++ game/src/pages/login/[[...index]].tsx
@@ -1,6 +1,5 @@
 import { useCallback, useEffect } from "react";
 import { useRouter } from "next/router";
-
 import type { NextPage } from "next";
 import { Layout } from "@/components/common/Layout";
 import { useUser } from "@clerk/nextjs";
@@ -9,11 +8,13 @@ import LoadingSpinner from "@/components/common/Loader/LoadingSpinner";
 
 const Login: NextPage = () => {
   const router = useRouter();
-  const { user } = useUser();
+  const { user, isLoaded: isUserLoaded } = useUser();
 
-  const { data: paPlayer, isLoading } = api.paUsers.getPlayerByNick.useQuery({
-    nick: user?.username || "",
-  });
+  const { data: paPlayer, isLoading: isPlayerLoading } =
+    api.paUsers.getPlayerByNick.useQuery(
+      { nick: user?.username || "" },
+      { enabled: !!user?.username },
+    );
 
   const addPlayer = useCallback(() => {
     return router.push("/addUser");
@@ -24,21 +25,37 @@ const Login: NextPage = () => {
   }, [router]);
 
   useEffect(() => {
-    if (!isLoading) {
-      if (paPlayer && paPlayer.id) {
-        void redirect();
+    if (isUserLoaded && !isPlayerLoading) {
+      if (user) {
+        if (paPlayer && paPlayer.id) {
+          void redirect();
+        } else {
+          void addPlayer();
+        }
       } else {
-        void addPlayer();
+        // User is not logged in, redirect to login page or show login form
+        // For example: router.push("/login");
       }
     }
-  }, [paPlayer, isLoading, redirect, addPlayer]);
+  }, [user, isUserLoaded, paPlayer, isPlayerLoading, redirect, addPlayer]);
 
+  if (!isUserLoaded || isPlayerLoading) {
+    return (
+      <Layout>
+        <div className="container mb-6 flex flex-col items-center justify-center">
+          <div className="mt-12">
+            <LoadingSpinner />
+          </div>
+        </div>
+      </Layout>
+    );
+  }
+
+  // This part will only be reached if there's an unexpected state
   return (
     <Layout>
       <div className="container mb-6 flex flex-col items-center justify-center">
-        <div className="mt-12">
-          <LoadingSpinner />
-        </div>
+        <div className="mt-12">Unexpected state. Please try again.</div>
       </div>
     </Layout>
   );
