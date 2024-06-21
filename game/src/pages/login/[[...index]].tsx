d6e39e7b86f5e78582b6cdb9f731502649ca0e60
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-21 03:29:10 +0200

    Update [[...index]].tsx

--- game/src/pages/login/[[...index]].tsx
+++ game/src/pages/login/[[...index]].tsx
@@ -4,6 +4,8 @@ import { useRouter } from "next/router";
 
 import type { NextPage } from "next";
 import { Layout } from "@/components/common/Layout";
+import { useUser } from "@clerk/nextjs";
+import { api } from "@/utils/api";
 
 /**
  * Renders the Login component with a sign-in form
@@ -12,22 +14,30 @@ import { Layout } from "@/components/common/Layout";
  */
 const Login: NextPage = () => {
   const router = useRouter();
+  const { user } = useUser();
+
+  const { data: paPlayer } = api.paUsers.getPlayerByNick.useQuery({
+    nick: user?.username || "",
+  });
 
   // wrapped in `useCallback` to avoid re-creating the function on each render
   const redirect = useCallback(async () => {
     return router.push("/addUser");
   }, [router]);
 
+  /*
   useEffect(() => {
     alert("Login test");
     void redirect();
   }, [router]);
+  */
 
   return (
     <>
       <Layout>
         <div className="container mb-6 flex flex-col items-center justify-center">
-          Login redirect
+          Login redirect Username: {user?.username} <br />
+          Pa player: {JSON.stringify(paPlayer)}
         </div>
       </Layout>
     </>
