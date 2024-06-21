9dcecf26314a5ccdcea0aadb5eca482f27360136
Daniel Fjeldstad <45217974+w3bdesign@users.noreply.github.com>
2024-06-21 04:18:08 +0200

    Update [[...index]].tsx

--- game/src/pages/login/[[...index]].tsx
+++ game/src/pages/login/[[...index]].tsx
@@ -1,11 +1,11 @@
-//import { SignIn, SignedOut } from "@clerk/nextjs";
-//import { useCallback, useEffect } from "react";
-//import { useRouter } from "next/router";
+import { useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
 
 import type { NextPage } from "next";
 import { Layout } from "@/components/common/Layout";
 import { useUser } from "@clerk/nextjs";
 import { api } from "@/utils/api";
+import LoadingSpinner from "@/components/common/Loader/LoadingSpinner";
 
 /**
  * Renders the Login component with a sign-in form
@@ -13,35 +13,35 @@ import { api } from "@/utils/api";
  * @return {JSX.Element} The Login component
  */
 const Login: NextPage = () => {
-  
-  //const router = useRouter();
+  const router = useRouter();
   const { user } = useUser();
 
   const { data: paPlayer } = api.paUsers.getPlayerByNick.useQuery({
     nick: user?.username || "",
   });
 
-
   // wrapped in `useCallback` to avoid re-creating the function on each render
-  /*
-  const redirect = useCallback(async () => {
+  const addPlayer = useCallback(async () => {
     return router.push("/addUser");
   }, [router]);
-  */
 
-  /*
-  useEffect(() => {
-    alert("Login test");
-    void redirect();
+  const redirect = useCallback(async () => {
+    return router.push("/");
   }, [router]);
-  */
+
+  useEffect(() => {
+    if (paPlayer && paPlayer.id) {
+      void addPlayer();
+    } else {
+      void redirect();
+    }
+  }, [paPlayer]);
 
   return (
     <>
       <Layout>
         <div className="container mb-6 flex flex-col items-center justify-center">
-          Login redirect Username: {user?.username} <br />
-          Pa player: {JSON.stringify(paPlayer)}
+          <LoadingSpinner />
         </div>
       </Layout>
     </>
