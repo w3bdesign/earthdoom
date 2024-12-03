8ea4a8b7e7dd9306acf732ef2bdcac67690cdb3d
w3bdesign <45217974+w3bdesign@users.noreply.github.com>
2024-12-03 05:43:13 +0100

    Update jest.config.js

--- game/jest.config.js
+++ game/jest.config.js
@@ -9,19 +9,22 @@ const customJestConfig = {
   testEnvironment: 'jest-environment-jsdom',
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1',
-    "^server/(.*)$": '<rootDir>/src/server/$1'
+    '^server/(.*)$': '<rootDir>/src/server/$1',
+    '^@prisma/client$': '<rootDir>/node_modules/@prisma/client',
   },
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  coverageReporters: ['text', 'lcov'],
-  collectCoverageFrom: [
-    'src/**/*.{js,jsx,ts,tsx}',
-    '!src/**/*.d.ts',
-    '!src/**/_*.{js,jsx,ts,tsx}',
-    '!src/**/*.stories.{js,jsx,ts,tsx}'
+  transform: {
+    '^.+\\.(ts|tsx|js|jsx|mjs)$': ['ts-jest', {
+      tsconfig: 'tsconfig.json',
+      useESM: true,
+    }]
+  },
+  transformIgnorePatterns: [
+    '/node_modules/(?!(@prisma|@clerk|superjson|@trpc)/)',
   ],
+  extensionsToTreatAsEsm: ['.ts', '.tsx', '.mjs'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'mjs'],
 };
 
 module.exports = createJestConfig(customJestConfig);
